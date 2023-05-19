b133e1d5de53f58c107f6cfa9df4cf7b558bfde9
annabusaga <anna.busaga@gmail.com>
2023-05-19 12:44:08 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
 export const App = () => {
   return (
-    <><h1></h1></>
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    </>
   );
 };
